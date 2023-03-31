fe520a3b4091d9f31d48dd6efd76ceef1c769128
Valery Maltsev <lisowsky@yandex.ru>
2023-03-31 10:21:03 +0100

    Applicants ready, Hogwarts ready + 4 класса факультетов работа над созданием массивов факультетов

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -9,7 +9,6 @@ public class Hogwarts {
         this.magicPower = (int) ((Math.random() * (100 - 1)) + 1);
         this.apparition = (int) ((Math.random() * (100 - 1)) + 1);
     }
-
     //Блок геттеров и сеттеров
     public Applicant getApplicant() {return applicant;}
     public int getMagicPower() {return magicPower;}
@@ -17,12 +16,10 @@ public class Hogwarts {
 
     @Override
     public String toString() {
-        return "Факультет " +applicant.getFaculty() +
-                ". Студент " +applicant.getName() +
-                ". Магическая сила " +magicPower+
+        return "Факультет " + applicant.getFaculty() +
+                ". Студент " + applicant.getName() +
+                ". Магическая сила " + magicPower +
                 ". Трансгрессия " + apparition;
-
-
     }
 
     public static class Griffyndor extends Hogwarts {
@@ -35,77 +32,115 @@ public class Hogwarts {
         public int getHonor() {return honor;}
         public int getBravery() {return bravery;}
 
-        public Griffyndor (Applicant applicant) {
-            super (applicant);
-            this.nobility = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.honor = (int) ((Math.random() * (100 - 1)) + 1);;
-            this. bravery = (int) ((Math.random() * (100 - 1)) + 1);;
-
+        public Griffyndor(Applicant applicant) {
+            super(applicant);
+            this.nobility = (int) ((Math.random() * (100 - 1)) + 1);
+            this.honor = (int) ((Math.random() * (100 - 1)) + 1);
+            this.bravery = (int) ((Math.random() * (100 - 1)) + 1);
         }
-    }
-    public static class Hufflepuff extends Hogwarts {
-        private final int diligence;
-        private final int loyalty;
-        private final int honesty;
-
-        //Блок геттеров, сеттеры не нужны
-        public int getDiligence() {return diligence;}
-        public int getLoyalty() {return loyalty;}
-        public int getHonesty() {return honesty;}
-
-        public Hufflepuff (Applicant applicant) {
-            super (applicant);
-            this.diligence = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.loyalty = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.honesty = (int) ((Math.random() * (100 - 1)) + 1);;
-
-        }
-    }
-    public static class RawenClow extends Hogwarts {
-        private final int intellect;
-        private final int wisdom;
-        private final int wit;
-        private final int creative;
-
-        //Блок геттеров, сеттеры не нужны
-        public int getIntellect() {return intellect;}
-        public int getWisdom() {return wisdom;}
-        public int getWit() {return wit;}
-        public int getCreative() {return creative;}
-
-        public RawenClow (Applicant applicant) {
-            super (applicant);
-            this.intellect = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.wisdom = (int) ((Math.random() * (100 - 1)) + 1);;
-            this. wit = (int) ((Math.random() * (100 - 1)) + 1);;
-            this. creative = (int) ((Math.random() * (100 - 1)) + 1);
-
+        @Override
+        public String toString() {
+            return "Факультет " + getApplicant().getFaculty() +
+                    ". Студент " + getApplicant().getName() +
+                    ". Благородство " + nobility +
+                    ". Честь " + honor +
+                    ". Храбрость " + bravery;
         }
     }
 
-    public static class Slytherin extends Hogwarts {
-        private final int trick;
-        private final int resolute;
-        private final int ambition;
-        private final int resourcefulness;
-        private final int powerthirst;
+        public static class Hufflepuff extends Hogwarts {
+            private final int diligence;
+            private final int loyalty;
+            private final int honesty;
 
-        //Блок геттеров, сеттеры не нужны
-        public int getTrick() {return trick;}
-        public int getResolute() {return resolute;}
-        public int getAmbition() {return ambition;}
-        public int getResourcefulness() {return resourcefulness;}
-        public int getPowerthirst() {return powerthirst;}
+            //Блок геттеров, сеттеры не нужны
+            public int getDiligence() {return diligence;}
+            public int getLoyalty() {return loyalty;}
+            public int getHonesty() {return honesty;}
 
-        public Slytherin (Applicant applicant) {
-            super (applicant);
-            this.trick = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.resolute = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.ambition = (int) ((Math.random() * (100 - 1)) + 1);;
-            this.resourcefulness = (int) ((Math.random() * (100 - 1)) + 1);
-            this.powerthirst = (int) ((Math.random() * (100 - 1)) + 1);
+            public Hufflepuff(Applicant applicant) {
+                super(applicant);
+                this.diligence = (int) ((Math.random() * (100 - 1)) + 1);
+                this.loyalty = (int) ((Math.random() * (100 - 1)) + 1);
+                this.honesty = (int) ((Math.random() * (100 - 1)) + 1);
+            }
 
+            @Override
+            public String toString() {
+                return "Факультет " + getApplicant().getFaculty() +
+                        ". Студент " + getApplicant().getName() +
+                        ". Трудолюбие " + diligence +
+                        ". Верность " + loyalty +
+                        ". Честность " + honesty;
+            }
         }
+
+        public static class RawenClow extends Hogwarts {
+            private final int intellect;
+            private final int wisdom;
+            private final int wit;
+            private final int creative;
+
+            //Блок геттеров, сеттеры не нужны
+            public int getIntellect() {return intellect;}
+            public int getWisdom() {return wisdom;}
+            public int getWit() {return wit;}
+            public int getCreative() {return creative;}
+
+            public RawenClow(Applicant applicant) {
+                super(applicant);
+                this.intellect = (int) ((Math.random() * (100 - 1)) + 1);
+                this.wisdom = (int) ((Math.random() * (100 - 1)) + 1);
+                this.wit = (int) ((Math.random() * (100 - 1)) + 1);
+                this.creative = (int) ((Math.random() * (100 - 1)) + 1);
+            }
+
+            @Override
+            public String toString() {
+                return "Факультет " + getApplicant().getFaculty() +
+                        ". Студент " + getApplicant().getName() +
+                        ". Ум " + intellect +
+                        ". Мудрость " + wisdom +
+                        ". Остроумие " + wit +
+                        ". Творчество " + creative;
+            }
+        }
+
+        public static class Slytherin extends Hogwarts {
+            private final int trick;
+            private final int resolute;
+            private final int ambition;
+            private final int resourcefulness;
+            private final int powerthirst;
+
+            //Блок геттеров, сеттеры не нужны
+            public int getTrick() {return trick;}
+            public int getResolute() {return resolute;}
+            public int getAmbition() {return ambition;}
+            public int getResourcefulness() {return resourcefulness;}
+            public int getPowerthirst() {return powerthirst;}
+
+            public Slytherin(Applicant applicant) {
+                super(applicant);
+                this.trick = (int) ((Math.random() * (100 - 1)) + 1);
+                this.resolute = (int) ((Math.random() * (100 - 1)) + 1);
+                this.ambition = (int) ((Math.random() * (100 - 1)) + 1);
+                this.resourcefulness = (int) ((Math.random() * (100 - 1)) + 1);
+                this.powerthirst = (int) ((Math.random() * (100 - 1)) + 1);
+            }
+
+            @Override
+            public String toString() {
+                return "Факультет " + getApplicant().getFaculty() +
+                        ". Студент " + getApplicant().getName() +
+                        ". Хитрость " + trick +
+                        ". Решительность " + resolute +
+                        ". Амбициозность " + ambition +
+                        ". Находчивость " + resourcefulness +
+                        ". Жажда власти " + powerthirst;
+            }
+        }
+
     }
 
 
@@ -118,6 +153,3 @@ public class Hogwarts {
 
 
 
-
-
-}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     static Applicant [] applicants = new Applicant[12];
     static Hogwarts[] hogwartsStudents = new Hogwarts[12];
+    static Hogwarts.Griffyndor[] griffyndorsStudents = new Hogwarts.Griffyndor[3];
+    static Hogwarts.Hufflepuff[] hufflepuffsStudents = new Hogwarts.Hufflepuff[3];
+    static Hogwarts.RawenClow[] rawenClowsStudents = new Hogwarts.RawenClow[3];
+    static Hogwarts.Slytherin[] slytherinsStudents = new Hogwarts.Slytherin[3];
 
     public static void main(String[] args) {
 
@@ -21,11 +25,39 @@ public class Main {
 
         // Зачисляем абитурьентов в Хогвардс тестируя и присваивая им общие для всех качества: силу магиии и трансгресии
         for (int i = 0; i < hogwartsStudents.length; i++) {
-            Hogwarts temporary = new Hogwarts(applicants[i]);
-            hogwartsStudents[i] = temporary;
-            System.out.println(hogwartsStudents[i]);
+            hogwartsStudents[i] = new Hogwarts(applicants[i]);
+            //System.out.println(hogwartsStudents[i]);
         }
         // Распределяем студентов согласно выбранных факультетов, тестируем и присваиваем величину уникальных характеристик.
+        griffyndorsStudents[0] = new Hogwarts.Griffyndor(applicants[0]);
+        System.out.println("griffyndorsStudents[0] = " + griffyndorsStudents[0]);
+
+        //for (int i = 0; i < applicants.length; i++) {
+        //int j = 0;
+        //    int k = 0;
+        //    int l = 0;
+        //    int m = 0;
+        //    if (applicants[i].getFaculty().equals("Gryffindor")) {
+        //        griffyndorsStudents[j] = new Hogwarts.Griffyndor(applicants[i]);
+        //        j++;
+        //        System.out.println(j);
+        //
+        //  } else if (applicants[i].getFaculty().equals("Hufflepuff")) {
+        //       hufflepuffsStudents[k] = new Hogwarts.Hufflepuff(applicants[i]);
+        //      k++;
+        //
+        //    } else if (applicants[i].getFaculty().equals("Raven claw")) {
+        //        rawenClowsStudents[l] = new Hogwarts.RawenClow(applicants[i]);
+        //        l++;
+        //
+        //    } else if (applicants[i].getFaculty().equals("Slytherin")) {
+        //        slytherinsStudents[m] = new Hogwarts.Slytherin(applicants[i]);
+        //        m++;
+        //    }
+            //System.out.println("griffyndorsStudents[2] = " + griffyndorsStudents[2]);
+
+
+        //}
 
     }
 }
